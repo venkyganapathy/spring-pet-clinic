9e18b9821f87ce410a7184bb0b3844d29314c68b
venkyganapathy <venkyg1972@gmail.com>
2019-02-01 12:57:40 +0530

    Added Map based service implementations for entities. Closes #6.

---
+++ spring-pet-data/src/main/java/maps/AbstractMapService.java
@@ -0,0 +1,32 @@
+package maps;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T, IDType> {
+
+    protected Map<IDType,T> map = new HashMap<IDType, T>();
+
+    Set<T> findAll(){
+        return new HashSet<T>(map.values());
+    }
+
+    T findById(IDType id){
+        return map.get(id);
+    }
+
+    T save(IDType id, T object){
+        map.put(id,object);
+        return object;
+    }
+
+    void deleteById(IDType id){
+        map.remove(id);
+    }
+
+    void delete(T object){
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+}
