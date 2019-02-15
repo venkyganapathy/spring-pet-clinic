5befaab49b6ffe2e407958eca86afa8f6f77fc84
venkyganapathy <venkyg1972@gmail.com>
2019-02-15 14:23:44 +0530

    Committing

---
+++ spring-pet-web/src/main/java/learner/venky/springpetclinic/controllers/ViewNames.java
@@ -0,0 +1,8 @@
+package learner.venky.springpetclinic.controllers;
+
+public class ViewNames {
+
+    public static final String index = "index";
+    public static final String vets = "vets/index";
+    public static final String owners = "owners/index";
+}
