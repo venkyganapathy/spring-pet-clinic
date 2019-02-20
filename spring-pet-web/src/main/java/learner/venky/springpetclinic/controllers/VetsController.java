068fc11f2c6e689e11b014a840a79ebba1d54390
venkyg1972@gmail.com <venkyg1972@gmail.com>
2019-02-20 11:04:40 +0530

    Closes #15. List Vets in Vets Index Page

--- spring-pet-web/src/main/java/learner/venky/springpetclinic/controllers/VetsController.java
+++ spring-pet-web/src/main/java/learner/venky/springpetclinic/controllers/VetsController.java
@@ -1,13 +1,24 @@
 package learner.venky.springpetclinic.controllers;
 
+import learner.venky.springpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
 
+    private VetService vetService;
+
+    @Autowired
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String getVets(){
+    public String getVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return ViewNames.vets;
     }
 }
